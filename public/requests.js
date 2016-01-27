d37d2c9417fedeaa4fd8e97f54c4f221d7b3d206
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-01-27 15:37:29 +0100

    Add json.stringify to data and json.parse to the response

--- public/requests.js
+++ public/requests.js
@@ -21,11 +21,11 @@ function Request() {
     var request = new XMLHttpRequest();
     request.open(method, url);
     request.setRequestHeader('Content-Type', 'application/json');
-    request.send(data);
+    request.send(data && JSON.stringify(data));
     request.onreadystatechange = function() {
       console.log('allapot: ', request.readyState);
       if (request.readyState === 4) {
-        callback(request.response);
+        callback(JSON.parse(request.response));
       }
     }
   };
