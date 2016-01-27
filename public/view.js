6d7761ddf80fd856cd457299af65a426d931e911
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-01-27 15:39:22 +0100

    Add delete button to meal items

--- public/view.js
+++ public/view.js
@@ -2,20 +2,15 @@
 
 function View() {
   var _this = this;
-  this.MealNameInput = document.querySelector('#food');
-  this.MealKcalInput = document.querySelector('#kcal');
-  this.MealDateInput = document.querySelector('#date');
-  this.addItemButton = document.querySelector('.add-item-button');
   this.list = document.querySelector('.meals');
 
   this.listMeals = function(response) {
-    var Meals = JSON.parse(response);
-    Meals.forEach(function(meal) { _this.addElements(meal) });
+    response.forEach(function(meal) { _this.addElements(meal) });
   };
 
   this.addElements = function(meal) {
-    var element = this.createMeals(meal);
-    this.list.innerHTML += element;
+    var element = _this.createMeals(meal);
+    _this.list.innerHTML += element;
   };
 
   this.createMeals = function(meal) {
@@ -23,12 +18,12 @@ function View() {
                      <td>${meal.name}</td>
                      <td>${meal.calories}</td>
                      <td>${this.dateFormat(meal.date)}</td>
+                     <td><button class="delete">Delete</button></td>
                    </tr>`;
     return element;
   };
 
   this.dateFormat = function(date) {
     return date.split('T')[0];
-  }
-
+  };
 }
