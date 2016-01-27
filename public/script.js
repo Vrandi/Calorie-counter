547b7a0354628100c1a7d417eaf662f330aef649
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-01-27 15:44:49 +0100

    Split script.js into controller.js and app.js

--- public/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function CalorieApp() {
-  this.request = new Request();
-  this.view = new View();
-  this.request.get(this.view.listMeals);
-}
-
-var calorieApp = new CalorieApp()
