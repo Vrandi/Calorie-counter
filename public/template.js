4f812eff7622ac0520493d54dd2213677c1279fb
Vrabély Andrea <vrabely.andrea@gmail.com>
2016-01-29 10:29:35 +0100

    add class property to calorie field

--- public/template.js
+++ public/template.js
@@ -3,9 +3,9 @@
 function template(item) {
   var element = `<tr id="${item.meals_id}" class="meal">
                      <td>${item.name}</td>
-                     <td>${item.calories}</td>
+                     <td class="calorie">${item.calories}</td>
                      <td class="meal-date">${dateFormat(item.date)}</td>
-                     <td><button class="delete">Delete</button></td>
+                     <td><button class="delete"><span class="glyphicon glyphicon-scissors"></span></button></td>
                    </tr>`;
   return element;
 }
